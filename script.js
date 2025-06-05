52153048f296e1a816711c254533e72f6c707909
nyagooh <annmaina614@gmail.com>
2025-06-05 17:37:43 +0300

    update link to cv

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ if (contactForm) {
 const downloadBtn = document.querySelector('.download-cv');
 if (downloadBtn) {
     downloadBtn.addEventListener('click', () => {
-        const cvUrl = 'public/mainaresume (3).pdf';
+        const cvUrl = 'public/Maina_Anne_resume.pdf';
         
         // Fetch the PDF file
         fetch(cvUrl)
